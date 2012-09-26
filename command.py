fd089ec7ca2f4efbc8ea629d8e8432a958640859
Wendell Antildes <wendellantildes@gmail.com>
2012-09-26 12:11:41 -0300

    Funcionalidades refatoradas: login,logout,exit,update_user_password. Gerado bug de autenticação

--- command.py
+++ command.py
@@ -1,23 +1,64 @@
-from UserAction import UserAction
+from prompt import Prompt
+from service import *
+from messages import *
+import sys
 
 class Invoker(object):
     
-    def __init__(self,addUserCommand):
+    def __init__(self,addUserCommand,updateUserPasswordCommand,loginCommand,logoutCommand,exitCommand):
         self.addUserCommand = addUserCommand
+        self.updateUserPasswordCommand = updateUserPasswordCommand
+
+        self.loginCommand = loginCommand
+        self.logoutCommand = logoutCommand
+        self.exitCommand = exitCommand
     
     def add_user(self):
         self.addUserCommand.execute()
+
+    def update_user_password(self):
+        self.updateUserPasswordCommand.execute()
+
+    def login(self):
+        self.loginCommand.execute()
+
+    def logout(self):
+        self.logoutCommand.execute()
+
+    def exit(self):
+        self.exitCommand.execute()
         
 class InvokerMaker(object):
     
     def __init__(self):
         self.addUserCommand = None
+        self.updateUserPasswordCommand = None
+        self.loginCommand = None
+        self.logoutCommand = None
+        self.exitCommand = None
         
-    def registerAddUserCommand(command)
+    def registerAddUserCommand(self,command):
         self.addUserCommand = command
+        return self
+
+    def registerUpdateUserPasswordCommand(self,command):
+        self.updateUserPasswordCommand = command
+        return self
+
+    def registerLoginCommand(self,command):
+        self.loginCommand = command
+        return self
+
+    def registerLogoutCommand(self,command):
+        self.logoutCommand = command
+        return self
+
+    def registerExitCommand(self,command):
+        self.exitCommand = command
+        return self
         
     def buildInvoker(self):
-        return Invoker(self.addUserCommand)
+        return Invoker(self.addUserCommand,self.updateUserPasswordCommand,self.loginCommand,self.logoutCommand,self.exitCommand)
     
     
 class Command(object):
@@ -30,8 +71,88 @@ class Command(object):
 
 class AddUserCommand(Command):
 
-    def __init__(self):
-        self.user_action = UserAction()
+    def __init__(self,prompt):
+        self.prompt = prompt
 
     def execute(self):
-        self.user_action.add_user()
+        self.add_user()
+
+
+    def add_user(self):
+        name = self.prompt.type_user_name()
+        valid          = False
+        userService = UserService()
+        email = self.prompt.type_user_email(userService)
+        password = self.prompt.type_user_password(prompt_command_password,prompt_command_password_again)
+        try:
+            userService.add(name=name,email=email,password=password)
+            self.prompt.print_message(prompt_command_user_added)
+        except IntegrityError:
+            #verificar qual o erro
+            self.print_message("erro")
+
+class UpdateUserPasswordCommand(Command):
+
+    def __init__(self,prompt,authentication):
+        self.prompt = prompt
+        self.authentication = authentication
+
+    def execute(self):
+        self.update_password()
+
+    def update_password(self):
+        new_password = self.prompt.type_user_password(password_message=prompt_command_update_user_type_password,password_message_again=messages.prompt_command_update_user_type_password_again)
+        user = self.authenticationService.user 
+        userService = UserService()
+        old_password  = self.prompt.type_password()
+        if self.authenticationService.password_is_right(old_password):
+            userService.update_password(user=user,old_password=old_password,new_password=new_password)
+            self.authenticationService.typed_password = new_password
+            self.prompt_print_message(prompt_command_update_user_password_updated)
+        else:
+            self.prompt.print_message(authentication_password_error)
+
+class ExitCommand(Command):
+
+    def __init__(self,prompt,authenticationService):
+        self.prompt = prompt
+        self.authenticationService = authenticationService
+
+    def execute(self):
+        if self.authenticationService.logged:
+            self.prompt.print_message(prompt_command_logout)
+        sys.exit()
+
+class LoginCommand(Command):
+
+    def __init__(self,prompt,authenticationService):
+        self.prompt = prompt
+        self.authenticationService = authenticationService
+
+    def execute(self):
+        security = Security()
+        email     = self.prompt.type_email()
+        password  = self.prompt.type_password()
+        if self.authenticationService.authenticate(email=email,password=password):
+            self.prompt.print_message(authentication_authenticated)
+            self.prompt.print_message(authentication_welcome.format(name=self.authenticationService.user.name))
+            info_session =  self.authenticationService.info_session()
+            self.prompt.print_message(authentication_session_begin % info_session[0].strftime("%d/%m/%y %H:%M"))
+            self.prompt.print_message(authentication_session_end % info_session[1].strftime("%d/%m/%y %H:%M"))
+            self.prompt.update_authentication_label(email)
+        else:
+            self.prompt.print_message(self.authenticationService.message_error)
+
+class LogoutCommand(Command):
+
+    def __init__(self,prompt,authenticationService):
+        self.prompt = prompt
+        self.authenticationService = authenticationService
+        
+    def execute(self):
+        if self.authenticationService.logged:
+            self.authenticationService.logout()
+            self.prompt.print_message(prompt_command_logout)
+            self.prompt.update_authentication_label("")
+        
+        
--- prompt.py
+++ prompt.py
@@ -1,4 +1,12 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*- 
 from command import *
+from messages import *
+import getpass
+import validate
+from datetime import datetime
+from sqlalchemy.exc import IntegrityError
+import re
 
 def raw_input_unicode(message):
     return unicode(raw_input(message),'utf8')
@@ -24,7 +32,7 @@ class login_required(object):
         
     def __get__(self, instance, owner):
         def wrapper(*args, **kwargs):
-            if instance.logged:
+            if instance.authenticationService.logged:
                 return self.f(instance,*args,**kwargs)
             else:
                 print prompt_command_login_required
@@ -37,14 +45,66 @@ class Prompt(object):
         print head
     
     def prompt(self):
-        try:
-            label_now =  "[{date}]{label}".format(date=datetime.now().strftime("%d/%m/%y %H:%M"),label=self.label)
-            return raw_input(label_now)
-        except KeyboardInterrupt:
-            print
+        label_now =  "[{date}]{label}".format(date=datetime.now().strftime("%d/%m/%y %H:%M"),label=self.label)
+        return raw_input(label_now)
             
-    def print_command_not_error(self):
-        print prompt_command_error
+    def update_authentication_label(self,email):
+        self.label = "{email}{label}".format(email=email,label=">>>")
+    
+    def type_user_name(self):
+        name = ""
+        valid = False
+        while(not valid):
+            name = raw_input_unicode(prompt_command_name)
+            if not validate.name(name):
+                print prompt_command_name_error
+            else:
+                valid = True
+        return name
+
+    def type_user_email(self,userService):
+        valid = False
+        email = ""
+        while(not valid):
+            email = raw_input_unicode(prompt_command_email)
+            if not validate.email(email):
+                print prompt_command_email_error
+            else:
+                if(userService.get_user(email) == None):
+                    valid = True
+                else:
+                    print prompt_command_email_duplicated
+        return email
+
+    def type_user_password(self,password_message,password_message_again):
+        password, password_again = "",""
+        count = 0
+        valid = False
+        while(not valid):
+            password       = unicode(getpass.getpass(password_message),'utf8')
+            password_again = unicode(getpass.getpass(password_message_again),'utf8')
+            if(password != password_again):
+                count +=1
+                if(count == 3):
+                    print prompt_command_operation_aborted
+                    return
+                print prompt_command_password_error
+            else:
+                if not validate.password(password):
+                    print prompt_command_password_error_empty
+                else:
+                    valid = True
+        return password
+    
+    def type_email(self):
+        return raw_input_unicode(prompt_command_login_email)
+
+    def type_password(self):
+        return getpass.getpass(prompt_command_login_password)
+        
+
+    def print_message(self,message):
+        print message
 
 class PromptManager(object):
     
@@ -55,31 +115,79 @@ class PromptManager(object):
         self.invoker = self.buildInvoker()
         self.options = {}
         self.functions = {}
+        self.build_options()
     
     def buildInvoker(self):
-        invokerMaker = InvokerMarker()
+        invokerMaker = InvokerMaker()
         addUserCommand = AddUserCommand(self.prompt)
-        invokerMaker.registerAddUserCommand(addUserCommand)
+        updateUserPasswordCommand = UpdateUserPasswordCommand(self.prompt,self.authenticationService)
+        invokerMaker.registerAddUserCommand(addUserCommand).registerUpdateUserPasswordCommand(updateUserPasswordCommand)
+
+        loginCommand = LoginCommand(self.prompt,self.authenticationService)
+        logoutCommand = LogoutCommand(self.prompt,self.authenticationService)
+        exitCommand = ExitCommand(self.prompt,self.authenticationService)
+        invokerMaker.registerExitCommand(exitCommand).registerLoginCommand(loginCommand).registerLogoutCommand(logoutCommand)
+       
         return invokerMaker.buildInvoker()
     
     def build_options(self):
         self.options["add_user"] = re.compile(r'add(\s)user(\s)*$')
         self.functions["add_user"] = self.add_user
+
+        self.options["add_update_user_password"] = re.compile(r'update(\s)password(\s)*$')
+        self.functions["add_update_user_password"] = self.update_user_password
+
+        self.options["exit"] = re.compile(r'exit(\s)*$')
+        self.functions["exit"] = self.exit
+    
+
+        self.options["login"] = re.compile(r'login(\s)*$')
+        self.functions["login"] = self.login
+            
+
+        self.options["logout"] = re.compile(r'logout(\s)*$')
+        self.functions["logout"] = self.logout
+
+        
     
     def add_user(self):
         self.invoker.add_user()
+
+    @login_required
+    def update_user_password(self):
+        self.invoker.update_user_password()
+
+    def login(self):
+        self.invoker.login()
+
+    def logout(self):
+        self.invoker.logout()
+        print self.authenticationService.logged
+
+    def exit(self):
+        self.invoker.exit()
         
     def run(self):
         while(True):
-            typed_line = self.prompt()
-            if len(typed_line.strip()) > 0:
-                self.operation(typed_line)
+            try:
+                typed_line = self.prompt.prompt()
+                if len(typed_line.strip()) > 0:
+                    self.operation(typed_line)
+            except KeyboardInterrupt:
+                self.prompt.print_message("")
+
                 
     def operation(self,typed_line):
         exists = False
-            for key in self.options.keys():
-                match = self.options[key].match(typed_line)
-                if match:
-                    self.functions[key]()
-            if not exists:
-                self.prompt.print_command_not_error
+        for key in self.options.keys():
+            match = self.options[key].match(typed_line)
+            if match:
+                self.functions[key]()
+                exists = True
+        if not exists:
+            self.prompt.print_message(prompt_command_error)
+
+
+if __name__ == "__main__":
+    prompt_manager = PromptManager()
+    prompt_manager.run()
